44b26431e6e7866f44b160103c3fc3b3bd81493b
smitm237 <127427436+smitm237@users.noreply.github.com>
2023-03-23 10:30:43 -0700

    Made beggining

--- battleship.c
+++ battleship.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -184,10 +183,47 @@ int main()
 {
     srand(time(NULL));
     
-    RandomPlaceAll(PLAYER);
+    
+    
+    RandomPlaceAll(OPPONENT);
     
     DrawGrid(PLAYER);
-
+    int i, ansPlace = 0, orientation, row;
+    char col;
+    
+    while(ansPlace != 1 && ansPlace != 2)
+    {    
+        printf("Type 1 to place your own ships or 2 for random ship placement: ");
+        scanf("%d", &ansPlace);
+    }
+    if(ansPlace == 1)
+    {
+        for(i=1; i=5; i<=5)
+        {
+            if(i == CARRIER)
+                printf("Place your Carrier (Size 5)\n");
+            else if(i = BATTLESHIP)
+                printf("Place your Battleship (Size 4)\n");
+            else if(i = CRUISER)
+                printf("Place your Cruiser (Size 3)\n");
+            else if(i = SUBMARINE)
+                printf("Place your Submarine (Size 3)\n");
+            else if(i = DESTROYER)
+                printf("Place your DESTROYER (Size 2)\n");
+            printf("Type 0 for Horizontal (right) Placement and 1 for Vertical (down) Placement: ");
+            scanf("%d", &orientation);
+            printf("Choose a Column: ");
+            scanf("%*c%c", &col);
+            printf("Choose a Row: ");
+            scanf("%d", &row);
+            PlaceShip(i, PLAYER, orientation, row-1, col-65);
+        }
+    }
+    else if(ansPlace == 2)
+    {
+        RandomPlaceAll(PLAYER);
+    }
+    DrawGrid(PLAYER);
     return 0;
 }
 
