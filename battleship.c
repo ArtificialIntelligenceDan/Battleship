94f60b7670b149cfb80006b03189e75eddf6b5be
smitm237 <127427436+smitm237@users.noreply.github.com>
2023-03-28 10:28:51 -0700

    Update battleship.c
    Player Turn

--- battleship.c
+++ battleship.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -149,8 +150,8 @@ void RandomPlaceAll(int grid){ // Place all ships at random
 }
 
 int Hit(int row, char col, int grid){ // Check if space has been hit
-    if (shipGrid[row][col-65][grid] != 0){
-        hitGrid[row][col-65][grid] = 1;
+    if (shipGrid[row][col][grid] != 0){
+        hitGrid[row][col][grid] = 1;
         return 1;
     }
     else{
@@ -182,23 +183,23 @@ int IsSunk(int ship, int grid){ // Check if ship has been sunk
 int main()
 {
     srand(time(NULL));
-    
-    
-    
+
+
+
     RandomPlaceAll(OPPONENT);
-    
+
     DrawGrid(PLAYER);
     int i, ansPlace = 0, orientation, row;
     char col;
-    
+
     while(ansPlace != 1 && ansPlace != 2)
-    {    
+    {
         printf("Type 1 to place your own ships or 2 for random ship placement: ");
         scanf("%d", &ansPlace);
     }
     if(ansPlace == 1)
     {
-        for(i=1; i=5; i<=5)
+        for(i=1; i<=5; i++)
         {
             if(i == CARRIER)
                 printf("Place your Carrier (Size 5)\n");
@@ -217,13 +218,37 @@ int main()
             printf("Choose a Row: ");
             scanf("%d", &row);
             PlaceShip(i, PLAYER, orientation, row-1, col-65);
-        }
-    }
+            DrawGrid(PLAYER);
+
+         }
+}
     else if(ansPlace == 2)
     {
         RandomPlaceAll(PLAYER);
+        DrawGrid(PLAYER);
     }
-    DrawGrid(PLAYER);
+
+    int hitRow;     // Player turn
+    char hitCol;
+int hit;
+ printf("Done\n");
+    DrawGrid(OPPONENT);
+
+    do{
+      printf("Choose a Target!\n");  //Player Turn
+      printf("Column: ");
+      scanf("%*c%c", &hitCol);
+      printf("Row: ");
+      scanf("%d", &hitRow);
+      hit = Hit(hitRow-1, hitCol-65, OPPONENT);
+      DrawHitGrid(PLAYER);
+      if(hit == 1){
+        printf("HIT!!!\n");
+      }
+      else if(hit == 0){
+        printf("MISS!!");
+      }
+     }while(hit == 1);
+
     return 0;
 }
-
