cb813cf7fcd0f86b016fa1e1203510f9a6a09ab3
Aidan <109261275+ArtificialIntelligenceDan@users.noreply.github.com>
2023-04-06 09:49:02 -0700

    added header comment

--- display.h
+++ display.h
@@ -1,3 +1,19 @@
+/*******************************************************************************
+* File: display.h
+* Dependencies: grids.h
+*
+* Purpose: Contains functions and variables used to display information on the 
+*          screen.
+* 
+* Course: CS125
+* Version:	1.0 - 4/3/23 - Aidan Kihm
+*           1.1 - 4/6/23 - Aidan Kihm
+*
+* Resources: 
+* https://textkool.com/en/ascii-art-generator?hl=default&vl=default&font=
+*   Red%20Phoenix&text=Your%20text%20here%20
+*******************************************************************************/
+
 void UpdateDisplayGrid(int); // Update the specified displayGrid array 
 
 void DisplayGrids(); // Display both grids on the screen side-by-side
